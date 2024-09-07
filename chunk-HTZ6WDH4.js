8d554b4225c78adbc0ed8082dbe1729446783314
dkeshri <dkeshridev@gmail.com>
2024-09-07 20:48:15 +0530

    Auto-generated commit

---
+++ chunk-HTZ6WDH4.js
@@ -0,0 +1,1 @@
+import{Q as a,ta as n,ua as o,xa as r,za as p}from"./chunk-WTZ2UCGR.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=a({type:t,selectors:[["app-area"]],standalone:!0,features:[p],decls:2,vars:0,template:function(e,m){e&1&&(n(0,"p"),r(1,"area works!"),o())}})};export{s as AreaComponent};
